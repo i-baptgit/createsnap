ef93b8a8334138eb9a98dc26d141f942c1237c6c
i-baptgit <91989701+i-baptgit@users.noreply.github.com>
2024-03-28 15:41:52 +0100

    Update scriptsnap.sh

--- scriptsnap.sh
+++ scriptsnap.sh
@@ -3,13 +3,13 @@
 # Get list of disk selfLinks
 disk_selfLinks=$(gcloud compute disks list --format="csv[no-heading](selfLink)")
 
-# Specify the existing snapshot policy name
-existing_snapshot_policy="schedule-1"
+# Specify the snapshot policy name
+snapshot_policy="mg1-sbx-prj-vms-magasins-dss"
 
 # Loop through each disk selfLink
 while IFS= read -r disk_selfLink; do
     # Apply existing snapshot policy to disk
     gcloud compute disks add-resource-policies $disk_selfLink \
-        --resource-policies=$existing_snapshot_policy
-    echo "Snapshot policy '$existing_snapshot_policy' applied to disk '$disk_selfLink'."
+        --resource-policies=$snapshot_policy
+    echo "Snapshot policy '$snapshot_policy' applied to disk '$disk_selfLink'."
 done <<< "$disk_selfLinks"
